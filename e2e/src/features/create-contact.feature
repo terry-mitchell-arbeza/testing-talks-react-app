7d4b48c4427899fdb048b2939df7cf2b6fe0e92a
Terry Mitchell <tmitchell@anzgcis.com>
2024-01-14 23:05:02 +1100

    section 6.47 first end to end test

--- e2e/src/features/create-contact.feature
+++ e2e/src/features/create-contact.feature
@@ -6,3 +6,20 @@ Feature: As a user I expect to be able to create contacts
     And I click the "create" button
     Then I am directed to the "create contact" page
     And the "create contact header" should contain the text "Create Contact"
+    And I fill in the "name" input with "Ted Smith"
+    And I select the "Male" option from the "gender"
+    And I fill in the "phone" input with "0403929282"
+    And I fill in the "street" input with "123 Fake St"
+    And I fill in the "city" input with "Melbourne"
+    And I click the "save" button
+    And I am directed to the "home" page
+    And I fill in the "search" input with "Ted Smith"
+    And the "full name label" should contain the text "Name:"
+    And the "name" should contain the text "Ted Smith"
+    And the "gender label" should contain the text "Gender:"
+    And the "gender" should contain the text "Male"
+    And the "address label" should contain the text "Address:"
+    And the "address" should contain the text "123 Fake St, Melbourne"
+    And the "edit" should be displayed
+    And the "delete" should be displayed
+    
---
+++ e2e/src/step-definitions/form.ts
@@ -0,0 +1,52 @@
+import {Then } from '@cucumber/cucumber'
+import {waitFor} from "../support/wait-for-behaviour";
+import { getElementLocator} from "../support/web-element-helper";
+import {ScenarioWorld} from "./setup/world";
+import {ElementKey} from "../env/global";
+import {inputValue, selectValue} from "../support/html-behaviour";
+
+Then(
+    /^I fill in the "([^"]*)" input with "([^"]*)"$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, input: string) {
+        const {
+            screen: { page },
+            globalConfig
+        } = this;
+
+        console.log(`I fill in the ${elementKey} input with ${input}`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            const result = await page.waitForSelector(elementIdentifier, {
+                state: 'visible'});
+            if(result){
+                await inputValue(page, elementIdentifier, input);
+            }
+            return result;
+        });
+    }
+)
+
+Then(
+    /^I select the "([^"]*)" option from the "([^"]*)"$/,
+    async function(this: ScenarioWorld, option: string, elementKey: ElementKey) {
+        const {
+            screen: { page },
+            globalConfig
+        } = this;
+
+        console.log(`I select the ${option} option from the ${elementKey}`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            const result = await page.waitForSelector(elementIdentifier, {
+                state: 'visible'});
+            if(result){
+                await selectValue(page, elementIdentifier, option);
+            }
+            return result;
+        });
+    }
+)
