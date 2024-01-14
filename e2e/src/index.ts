4877d44d44cc8f664850985aa9d975d737664be3
Terry Mitchell <tmitchell@anzgcis.com>
2024-01-14 19:24:06 +1100

    section 5.30 hosts and pages json

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -1,13 +1,32 @@
 import dotenv from 'dotenv';
-import { env } from './env/parseEnv';
+import { env, getJsonFromFile } from './env/parseEnv';
+import {
+    GlobalConfig,
+    HostsConfig,
+    PagesConfig
+} from "./env/global";
 
 dotenv.config({ path: env('COMMON_CONFIG_FILE')});
 
+const hostsConfig: HostsConfig = getJsonFromFile(env('HOSTS_URLS_PATH'));
+
+console.log('hostsConfig', hostsConfig);
+
+const pagesConfig: PagesConfig = getJsonFromFile(env('PAGE_URLS_PATH'))
+
+console.log('pagesConfig', pagesConfig);
+
+const worldParameters: GlobalConfig = {
+    hostsConfig,
+    pagesConfig
+}
+
 const common = `./src/features/**/*.feature \
-                                        --require-module ts-node/register \
-                                        --require ./src/step-definitions/**/**/*.ts \
-                                        -f json:./reports/report.json \
-                                        --format progress-bar`;
+                        --require-module ts-node/register \
+                        --require ./src/step-definitions/**/**/*.ts \
+                        --world-parameters ${JSON.stringify(worldParameters)} \
+                        -f json:./reports/report.json \
+                        --format progress-bar`;
 
 const dev = `${common} --tags '@dev'`;
 const smoke = `${common} --tags '@smoke'`;
--- e2e/src/step-definitions/setup/world.ts
+++ e2e/src/step-definitions/setup/world.ts
@@ -7,6 +7,7 @@ import playwright, {
 } from "playwright";
 import {env} from "../../env/parseEnv";
 import {World, IWorldOptions, setWorldConstructor} from "@cucumber/cucumber" ;
+import { GlobalConfig } from "../../env/global";
 
 export type Screen = {
     browser: Browser;
@@ -17,7 +18,11 @@ export type Screen = {
 export class ScenarioWorld extends World {
     constructor(options: IWorldOptions) {
         super(options)
+
+        this.globalConfig = options.parameters as GlobalConfig;
     }
+
+    globalConfig: GlobalConfig;
 
     screen!: Screen;
 
